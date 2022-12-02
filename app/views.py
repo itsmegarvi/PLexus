b4c9e9ba1e56f6647f040b35269f36c7e99c7206
Garvit Shubham <mailme.garvit@gmail.com>
2022-12-03 00:37:30 +0530

    feat(app/views.py) add new view

--- app/views.py
+++ app/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django import forms
+import pickle
 
+model = pickle.load(open('model_pkl1', 'rb'))
 # Create your views here.
+
+
+def index(request):
+    return HttpResponse("hello,welcome to my attempt to learn development.")
+
+
+def predict(request):
+    int_features = [request.GET.get('date'), 15]
+    outcome = model.predict([[25, 11, 8, 35, 4]])
+    return HttpResponse(outcome)
